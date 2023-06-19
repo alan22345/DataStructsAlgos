af69878e1af46bcc127a5be2b8de9c82c19dc1b9
Alan Yeginchibayev <alayegin@WKMZTB5DBBB6.global.publicisgroupe.net>
2023-06-19 12:10:40 +0100

    addition

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -11,3 +11,45 @@ def even_odd (A) :
 
     return A
 
+# Write a program that takes an array A and an index i into A, and rearranges the elements such
+# that all elements less than A[r] (the "pivot") appear first, followed by elements equal to the pivot,
+# followed by elements greater than the pivot.
+
+def dutch_flag_partition(pivot_index, A):
+    pivot = A[pivot_index]
+    smaller, equal, larger = 0,0,len(A)
+
+    while equal < larger:
+        if A[equal] < pivot:
+            A[smaller], A[equal] = A[equal], A[smaller]
+            smaller, equal = smaller + 1, equal + 1
+        elif A[equal] == pivot:
+            equal += 1
+        else:
+            larger -= 1
+            A[equal], A[larger] = A[larger], A[equal]
+    return A
+
+a = [-999,1,23,4,5,543,431,-3,-4,-531,0,12,305,97]
+
+# Write a program which takes as input an array of digits encoding a nonnegative decimal integer
+# D and updates the array to represent the integer D + 1. For example, if the input is (7,2,9) then
+# you should update the array to (1,3,0). Your algorithm should work even if it is implemented in a
+# language that has finite-precision arithmetic.
+
+def plus_one(A):
+    A[-1] += 1
+    for i in reversed(range(1,len(A))):
+        if A[i] != 10:
+            break 
+        A[i] = 0
+        A[i-1] += 1
+    if A[0] == 10:
+        A[0] = 1
+        A.append(1)
+    return A
+
+print(plus_one([2,0,0,0]))
+
+# we can use a similar algo with a few extra steps for multiplying numbers 
+
